427984bcbb1806a92f631d262a2b98155a8ed231
kaprids <3298364642@qq.com>
2026-05-24 02:31:51 +0800

    fix: remove node-fetch, use native fetch

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import fetch from 'node-fetch';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import fs from 'fs';
